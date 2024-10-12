5a165a721dc4ef5a88d6117acd73a541c48190e2
Ashfat Al Rashid <61910244+AshfatAbegh@users.noreply.github.com>
2024-10-12 19:37:26 +0600

    Updated JSONSChemaValidation.java

--- src/test/java/Day6/JSONSChemaValidation.java
+++ src/test/java/Day6/JSONSChemaValidation.java
@@ -1,22 +1,22 @@
-// package Day6;
+package Day6;
 
-// import org.testng.annotations.Test;
+import org.testng.annotations.Test;
 
-// import static io.restassured.RestAssured.*;
-// import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
-// import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath; // Import the missing class
+import io.restassured.module.jsv.JsonSchemaValidator; 
 
-// public class JSONSChemaValidation {
+public class JSONSChemaValidation {
 
-//     @Test
-//     void JSONSchemaValidation() {
-//         given()
-//         .when()
-//            .get("https://jsonplaceholder.typicode.com/users")
-//         .then()
-//            .assertThat().body(matchesJsonSchemaInClasspath("JSONSchema/JSONSchema.json"));
-//     }
-// }
+    @Test
+    void JSONSchemaValidation() {
+        given()
+        .when()
+           .get("https://jsonplaceholder.typicode.com/users")
+        .then()
+           .assertThat().body(matchesJsonSchemaInClasspath("JSONSchema.json"));
+    }
+}
 
 
